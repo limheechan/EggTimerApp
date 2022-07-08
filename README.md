aa632fa665a9ffa4b833fc3e8ffb5fb13f71d111
limheechan <decodeode@gmail.com>
2022-07-08 23:04:21 +0900

    계란 타이머 완성

--- README.md
+++ README.md
@@ -6,3 +6,26 @@ Label 에 Lines 수를 0 으로 하면 글자 수 의 제한이 없다.
 
 Autoshrink 를 Minimum Font Size 로 한후 최소 라인 수를 결정 하여 그 최소줄 까지 내려갈 수 있다.
 
+    @IBAction func eggButtonPressed(_ sender: UIButton) {
+        print(sender.currentTitle)
+    }
+    
+    currentTitle 로 버튼의 타이틀을 확인 할 수 있다. 
+    
+
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true) 
+        타이머 인터벌 1초 간격 으로 반복 true selector 함수 <- 오브젝트 씨 의 잔재 
+            @objc func updateTimer() {
+        if secondsRemaining > 0 {
+            print("\(secondsRemaining) seconds.")
+            secondsRemaining -= 1
+        }
+    }
+    
+    해당 어플을 연습하다 보니 이상한 문제가 있는걸 확인했다. 
+    
+    먼저 한 차례 계란 타이머를 돌리고 난 후 다른 계란 타이머를 하면  마지막 시간을 이어 받은 상태로 진행한다.
+    
+    예를 들어  3초 후 알람이 울리고 다음 7초 짜리를 하면 3초부터 3....4...5...6..7 알람! 이런식이다 . 
+    이는 내 착각이였다. timer.invalidate() 가 타이머를 종료 시켜준다 하여 저 지나는 초 마저 0으로 초기화 하는줄 알았다. 
+    끝나고 나면 secondPassed 를 ㅔ\0 으로 초기화 하면 정상적으로 작동한다. 
--- eggTimer/ViewController.swift
+++ eggTimer/ViewController.swift
@@ -6,14 +6,77 @@
 //
 
 import UIKit
-
+import AVFAudio
 class ViewController: UIViewController {
+    var player: AVAudioPlayer!
+    
+    let eggTimes = ["Soft" : 3, "Medium" : 4, "Hard" : 7 ]
+    
+    //var secondsRemaining = 60
+    var totalTime = 0
+    var secondsPassed = 0
+    
+   var  timer = Timer()
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var progress: UIProgressView!
+    
+    
+    @IBAction func eggButtonPressed(_ sender: UIButton) {
+        
+        //타이머 중지 왜? 이걸 사용하느냐 처음 누른 버튼에서 이미 타이머가 진행 되고 있다 그때. 다른 버튼을 누루면 기존 타이머의 기능을 제거 하기 위함 으로 보인다.
+        timer.invalidate()
+        
+        let hardness = sender.currentTitle!
+        totalTime = eggTimes[hardness]!
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        //현재 남은 시간 % 1.0
+        //progress.progress =
+//        switch time {
+//            case 5:
+//            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 5) {
+//                self.playSound()
+//            }
+//            case 7:
+//            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 7) {
+//                self.playSound()
+//            }
+//            case 12:
+//            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 12) {
+//                self.playSound()
+//            }
+//            default:
+//                print("err")
+//        }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @objc func updateTimer() {
+        if secondsPassed < totalTime  {
+            
+            let percentProgress = Float(secondsPassed) / Float(totalTime)// why result is 0 ?
+            print(percentProgress)
+            progress.progress = Float( percentProgress)
+            secondsPassed += 1
+            titleLabel.text = "\( totalTime - secondsPassed ) 초 남았습니다."
+            
+        } else {
+            timer.invalidate()
+            titleLabel.text = "🥚완🥚성🥚"
+            progress.progress = 1.0
+            playSound()
+            secondsPassed = 0
+        }
+    }
+    
+    func playSound() {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+       
+    }
+    
 }
 
